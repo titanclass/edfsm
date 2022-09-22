cf9e95cd10a9b7ea1df8b1bde49f2c88e519a6c1
huntc <huntchr@gmail.com>
2022-09-22 11:14:38 +1000

    A little more doco

--- event_driven_macros/src/lib.rs
+++ event_driven_macros/src/lib.rs
@@ -5,6 +5,52 @@ mod parse;
 use proc_macro_error::{abort_call_site, proc_macro_error};
 use syn::parse2;
 
+/// Provides a DSL that conveniently implements the FSM trait.
+/// States, Commands and Events are all required to be implemented
+/// both as structs and enums.
+///
+/// An example:
+///
+/// ```compile_fail
+/// #[impl_fsm]
+/// impl Fsm<State, Command, Event, EffectHandlers> for MyFsm {
+///     state!(Running / entry);
+///     state!(Running / exit);
+///
+///     transition!(Idle    => Start => Started => Running);
+///     transition!(Running => Stop  => Stopped => Idle);
+/// }
+/// ```
+///
+/// The `state!` macro declares state-related attributes. At this time, entry and exit
+/// handlers can be declared. In our example, the macro will ensure that an `on_entry_running`
+/// and an `on_exit_running` method will be called for `MyFsm`. The developer is then
+/// required to implement these methods e.g.:
+///
+/// ```compile_fail
+/// fn on_exit_running(_old_s: &Running, _se: &mut EffectHandlers) {
+///     // Do something
+/// }
+/// ```
+///
+/// The `transition!` macro declares an entire transition using the form:
+///
+/// ```compile_fail
+/// <from-state> => <given-command> [=> <yields-event> []=> <to-state>]]
+/// ```
+///
+/// In our example, for the first transition, multiple methods will be called that the developer must provide e.g.:
+///
+/// ```compile_fail
+/// fn for_idle_start(_s: &Idle, _c: Start, _se: &mut EffectHandlers) -> Option<Started> {
+///     // Perform some effect here if required. Effects are performed via the EffectHandler
+///     Some(Started)
+/// }
+///
+/// fn on_idle_started(_s: &Idle, _e: &Started) -> Option<Running> {
+///     Some(Running)
+/// }
+/// ```
 #[proc_macro_attribute]
 #[proc_macro_error]
 pub fn impl_fsm(input: TokenStream, annotated_item: TokenStream) -> TokenStream {
