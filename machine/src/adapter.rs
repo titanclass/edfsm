c2b738993877cb5695923ed53ee3a6da7a007f6a
Arnold deVos <adv+gh@backgroundsignal.com>
2024-11-06 09:17:12 +1100

    add Send annotations

--- machine/src/adapter.rs
+++ machine/src/adapter.rs
@@ -9,7 +9,7 @@ pub trait Adapter {
 
     /// Forward the given item to an asynchronous consumer, possibly converting the type
     /// or possibly dropping the item if it cannot be converted.
-    fn notify(&mut self, a: Self::Item) -> impl Future<Output = Result<()>>
+    fn notify(&mut self, a: Self::Item) -> impl Future<Output = Result<()>> + Send
     where
         Self::Item: Clone + 'static;
 
@@ -30,10 +30,11 @@ pub trait Adapter {
     }
 
     /// Combine two adapters. The notify call is delegated to both adapters.
-    fn merge<T>(self, other: T) -> impl Adapter<Item = Self::Item>
+    fn merge<T>(self, other: T) -> impl Adapter<Item = Self::Item> + Send
     where
-        T: Adapter<Item = Self::Item>,
-        Self: Sized,
+        T: Adapter<Item = Self::Item> + Send,
+        Self: Sized + Send,
+        Self::Item: Send,
     {
         Merge {
             first: self,
@@ -43,10 +44,14 @@ pub trait Adapter {
 
     /// Create an adapter that maps items with an optional function.
     /// `Some` values are passed on, analogous to `Iterator::filter_map`.
-    fn adapt_filter_map<A>(self, func: impl Fn(A) -> Option<Self::Item>) -> impl Adapter<Item = A>
+    fn adapt_filter_map<A>(
+        self,
+        func: impl Fn(A) -> Option<Self::Item> + Send,
+    ) -> impl Adapter<Item = A> + Send
     where
-        Self: Sized,
-        Self::Item: Clone + 'static,
+        Self: Sized + Send,
+        Self::Item: Clone + Send + 'static,
+        A: Send,
     {
         FilterMap {
             func,
@@ -56,30 +61,31 @@ pub trait Adapter {
     }
 
     /// Create an adapter that maps each item with a function.
-    fn adapt_map<A>(self, func: impl Fn(A) -> Self::Item) -> impl Adapter<Item = A>
+    fn adapt_map<A>(self, func: impl Fn(A) -> Self::Item + Send) -> impl Adapter<Item = A> + Send
     where
-        Self: Sized,
-        Self::Item: Clone + 'static,
+        Self: Sized + Send,
+        Self::Item: Clone + Send + 'static,
+        A: Send,
     {
         self.adapt_filter_map(move |a| Some(func(a)))
     }
 
     /// Create an adapter that converts each item from another type.
-    fn adapt_from<A>(self) -> impl Adapter<Item = A>
+    fn adapt_from<A>(self) -> impl Adapter<Item = A> + Send
     where
-        Self: Sized,
-        Self::Item: Clone + 'static,
-        A: Into<Self::Item>,
+        Self: Sized + Send,
+        Self::Item: Clone + Send + 'static,
+        A: Into<Self::Item> + Send,
     {
         self.adapt_filter_map::<A>(move |a| Some(a.into()))
     }
 
     /// Create an adapter that fallibly converts each item from another type.
-    fn adapt_try_from<A>(self) -> impl Adapter<Item = A>
+    fn adapt_try_from<A>(self) -> impl Adapter<Item = A> + Send
     where
-        Self: Sized,
-        Self::Item: Clone + 'static,
-        A: TryInto<Self::Item>,
+        Self: Sized + Send,
+        Self::Item: Clone + Send + 'static,
+        A: TryInto<Self::Item> + Send,
     {
         self.adapt_filter_map::<A>(move |a| a.try_into().ok())
     }
@@ -95,7 +101,10 @@ impl<A> Default for Discard<A> {
     }
 }
 
-impl<A> Adapter for Discard<A> {
+impl<A> Adapter for Discard<A>
+where
+    A: Send,
+{
     type Item = A;
 
     /// Discard the item
@@ -121,8 +130,9 @@ pub struct Merge<S, T> {
 
 impl<E, S, T> Adapter for Merge<S, T>
 where
-    S: Adapter<Item = E>,
-    T: Adapter<Item = E>,
+    S: Adapter<Item = E> + Send,
+    T: Adapter<Item = E> + Send,
+    E: Send,
 {
     type Item = E;
 
@@ -146,9 +156,10 @@ pub struct FilterMap<A, F, G> {
 
 impl<F, G, A, B> Adapter for FilterMap<A, F, G>
 where
-    F: Fn(A) -> Option<B>,
-    B: Clone + 'static,
-    G: Adapter<Item = B>,
+    F: Fn(A) -> Option<B> + Send,
+    B: Clone + Send + 'static,
+    G: Adapter<Item = B> + Send,
+    A: Send,
 {
     type Item = A;
 
@@ -163,13 +174,32 @@ where
     }
 }
 
-/// A `Adapter` that forwards messages to an mpsc channel.
+/// Implement `Adapter` for a vector
+#[cfg(feature = "std")]
+impl<A> Adapter for std::vec::Vec<A>
+where
+    A: Send,
+{
+    type Item = A;
+
+    async fn notify(&mut self, a: Self::Item) -> Result<()>
+    where
+        Self::Item: Clone + 'static,
+    {
+        Ok(self.push(a))
+    }
+}
+
+/// Implementations of  `Adapter` for tokio channels.
 #[cfg(feature = "tokio")]
 pub mod adapt_channel {
     use crate::{adapter::Adapter, error::Result};
     use tokio::sync::{broadcast, mpsc};
 
-    impl<A> Adapter for mpsc::Sender<A> {
+    impl<A> Adapter for mpsc::Sender<A>
+    where
+        A: Send,
+    {
         type Item = A;
 
         async fn notify(&mut self, a: Self::Item) -> Result<()> {
@@ -178,7 +208,10 @@ pub mod adapt_channel {
         }
     }
 
-    impl<A> Adapter for broadcast::Sender<A> {
+    impl<A> Adapter for broadcast::Sender<A>
+    where
+        A: Send,
+    {
         type Item = A;
 
         async fn notify(&mut self, a: Self::Item) -> Result<()> {
